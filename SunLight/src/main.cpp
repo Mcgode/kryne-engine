52de8720e1d94aa52c874d6b0f5d971835ee2e5f
Max Godefroy (Bragg2X) <max@godefroy.net>
2019-04-19 18:52:18 +0200

    Initial Commit

---
+++ SunLight/src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by max on 19/04/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+    cout << "Hello World !" << endl;
+    return 0;
+}
