37ab3f4442c1a5343751a61157dd04cc7daa5c04
Max Godefroy (Bragg2X) <max@godefroy.net>
2019-05-21 11:44:51 +0200

    main - Base render of sphere

--- Sphere/main.cpp
+++ Sphere/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <common/Scene.h>
 #include <common/BasicCamera.h>
+#include <common/GeometricShapes.h>
 
 using namespace std;
 
@@ -13,9 +14,23 @@ int main(int argc, const char **argv)
 {
     auto *pScene = new Scene(new BasicCamera(glm::vec3(0, 0, -2), glm::vec3(0, 0, 0)));
 
+    vector<glm::vec3> vertices{};
+    generateSphereShape(0.5, 100, 50, &vertices);
+
+    auto *sh = new Shader("Quad/Color");
+    auto *va = new VertexArray(&vertices);
+    auto *obj = new BaseObject(sh, va);
+    pScene->addDrawable(new HierarchicalNode(obj));
+
+    sh->use();
+    sh->setVec3("color", 1.0, 0.0, 0.0);
+    Shader::resetUse();
+
     pScene->runLoop(nullptr);
 
-    delete(pScene);
+    delete sh;
+    delete va;
+    delete pScene;
 
     return 0;
 }
