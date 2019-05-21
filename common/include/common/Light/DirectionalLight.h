be49122e73a83f9b06cfc6f0a01e10033c2e499e
Max Godefroy (Bragg2X) <max@godefroy.net>
2019-05-21 20:12:00 +0200

    Documentation

--- common/include/common/Light/DirectionalLight.h
+++ common/include/common/Light/DirectionalLight.h
@@ -8,25 +8,40 @@
 #include <glm/vec3.hpp>
 #include <common/Shader.h>
 
-
+/**
+ * A wrapper for a light that shines in a given direction, independently of the distance
+ */
 class DirectionalLight {
 
 public:
 
+    /**
+     * A wrapper for a light that shines in a given direction, independently of the distance
+     * @param direction     The vector representing the direction of the light
+     * @param diffuseColor  The diffuse color of the light, defaults to white (#FFFFFF)
+     * @param ambientColor  The ambient color of the light, defaults to 0.1 * diffuseColor
+     * @param specularColor The specular color of the light, defaults to the diffuse color
+     */
     explicit DirectionalLight(glm::vec3 direction,
                               glm::vec3 diffuseColor = glm::vec3(1),
                               glm::vec3 *ambientColor = nullptr,
                               glm::vec3 *specularColor = nullptr);
 
+    /**
+     * Sets up all the light properties in the fragment shader
+     * @param shader        The shader program where you want to apply the light properties
+     * @param uniformName   The name of the uniform compiling all the light properties
+     */
     void shaderSet(Shader *shader, std::string uniformName);
 
 private:
 
+    /// The vector of the light direction
     glm::vec3 direction{};
 
-    glm::vec3 ambientColor{};
-    glm::vec3 diffuseColor{};
-    glm::vec3 specularColor{};
+    glm::vec3 ambientColor{}; ///< The ambient color of the light
+    glm::vec3 diffuseColor{}; ///< The diffuse color of the light
+    glm::vec3 specularColor{}; ///< The specular color of the light
 };
 
 
--- common/include/common/Light/PointLight.h
+++ common/include/common/Light/PointLight.h
@@ -10,36 +10,73 @@
 #include <vector>
 #include <algorithm>
 
+
+/**
+ * A wrapper for a light that shines from a provided position, with a potential attenuation
+ */
 class PointLight {
 
 public:
 
+    /**
+     * A wrapper for a light that shines from a provided position, with a potential attenuation
+     * @param direction     The vector representing the direction of the light
+     * @param diffuseColor  The diffuse color of the light, defaults to white (#FFFFFF)
+     * @param ambientColor  The ambient color of the light, defaults to 0.1 * diffuseColor
+     * @param specularColor The specular color of the light, defaults to the diffuse color
+     */
     explicit PointLight(glm::vec3 position,
                         glm::vec3 diffuseColor = glm::vec3(1),
                         glm::vec3 *ambientColor = nullptr,
                         glm::vec3 *specularColor = nullptr);
 
+    /**
+     * Sets the attenuation parameters
+     * @param constant  The constant attenuation of the light
+     * @param linear    The linear attenuation of the light
+     * @param quadratic The quadratic attenuation of the light
+     */
     void setAttenuation(float constant, float linear, float quadratic);
 
+    /**
+     * Sets up all the light properties in the fragment shader
+     * @param shader        The shader program where you want to apply the light properties
+     * @param uniformName   The name of the uniform compiling all the light properties
+     */
     void shaderSet(Shader *shader, std::string uniformName);
 
+    /**
+     * Sets up all the light properties in the fragment shader at a certain index in an array
+     * @param shader            The shader program where you want to apply the light properties
+     * @param uniformArrayName  The name of the uniform array
+     * @param index             The index of the light properties in the array
+     */
     void shaderSetArray(Shader *shader, std::string uniformArrayName, size_t index);
 
+    /**
+     * Retrieves a maximum of points closest to a given position
+     * @param pointLightList    The list of point lights to choose from
+     * @param position          The position to which the points must be the closest possible
+     * @param maximumAmount     The limit amount of point lights to retrieve
+     * @return A vector containing the n (with n <= maximumAmount) closest point lights to the provided position from
+     *         the input vector
+     */
     static std::vector<PointLight*> getClosestPointLights(const std::vector<PointLight*>& pointLightList,
                                                           glm::vec3 position,
                                                           size_t maximumAmount);
 
 private:
 
+    /// The position of the point light
     glm::vec3 position{};
 
-    glm::vec3 ambientColor{};
-    glm::vec3 diffuseColor{};
-    glm::vec3 specularColor{};
+    glm::vec3 ambientColor{}; ///< The ambient color of the light
+    glm::vec3 diffuseColor{}; ///< The diffuse color of the light
+    glm::vec3 specularColor{}; ///< The specular color of the light
 
-    float constantAttenuation{};
-    float linearAttenuation{};
-    float quadraticAttenuation{};
+    float constantAttenuation{}; ///< The constant attenuation of the light
+    float linearAttenuation{}; ///< The linear attenuation of the light
+    float quadraticAttenuation{}; ///< The quadratic attenuation of the light
 };
 
 
