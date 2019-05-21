dafabf477b87357b89f9db866faa4f3d35138b0d
Max Godefroy (Bragg2X) <max@godefroy.net>
2019-05-21 19:48:08 +0200

    PointLight - Implementation

---
+++ common/include/common/Light/PointLight.h
@@ -0,0 +1,44 @@
+//
+// Created by max on 21/05/19.
+//
+
+#ifndef INC_3D_DEMOS_POINTLIGHT_H
+#define INC_3D_DEMOS_POINTLIGHT_H
+
+#include <glm/vec3.hpp>
+#include <common/Shader.h>
+#include <vector>
+#include <algorithm>
+
+class PointLight {
+
+public:
+
+    explicit PointLight(glm::vec3 position,
+                        glm::vec3 diffuseColor = glm::vec3(1),
+                        glm::vec3 *ambientColor = nullptr,
+                        glm::vec3 *specularColor = nullptr);
+
+    void shaderSet(Shader *shader, std::string uniformName);
+
+    void shaderSetArray(Shader *shader, std::string uniformArrayName, size_t index);
+
+    static std::vector<PointLight*> getClosestPointLights(std::vector<PointLight*> pointLightList,
+                                                          glm::vec3 position,
+                                                          size_t maximumAmount);
+
+private:
+
+    glm::vec3 position{};
+
+    glm::vec3 ambientColor{};
+    glm::vec3 diffuseColor{};
+    glm::vec3 specularColor{};
+
+    float constantAttenuation{};
+    float linearAttenuation{};
+    float quadraticAttenuation{};
+};
+
+
+#endif //INC_3D_DEMOS_POINTLIGHT_H
---
+++ common/src/Light/PointLight.cpp
@@ -0,0 +1,59 @@
+//
+// Created by max on 21/05/19.
+//
+
+#include "common/Light/PointLight.h"
+
+PointLight::PointLight(glm::vec3 position, glm::vec3 diffuseColor, glm::vec3 *ambientColor, glm::vec3 *specularColor)
+{
+    this->position = position;
+    this->diffuseColor = diffuseColor;
+    this->ambientColor = ambientColor ? *ambientColor : 0.1f * this->diffuseColor;
+    this->specularColor = specularColor ? *specularColor : this->diffuseColor;
+}
+
+
+void PointLight::shaderSet(Shader *shader, std::string uniformName)
+{
+    shader->setVec3(uniformName.append(".position"), position);
+
+    shader->setVec3(uniformName.append(".ambient"), ambientColor);
+    shader->setVec3(uniformName.append(".diffuse"), diffuseColor);
+    shader->setVec3(uniformName.append(".specular"), specularColor);
+
+    shader->setFloat(uniformName.append(".constant"), constantAttenuation);
+    shader->setFloat(uniformName.append(".linear"), linearAttenuation);
+    shader->setFloat(uniformName.append(".quadratic"), quadraticAttenuation);
+}
+
+
+void PointLight::shaderSetArray(Shader *shader, std::string uniformArrayName, size_t index)
+{
+    shaderSet(shader, uniformArrayName.append("[").append(std::to_string(index)).append("]"));
+}
+
+
+std::vector<PointLight*> PointLight::getClosestPointLights(std::vector<PointLight*> pointLightList,
+                                                          glm::vec3 position,
+                                                          size_t maximumAmount)
+{
+    std::vector<PointLight *> v;
+
+    for (PointLight *light : pointLightList) {
+        PointLight *l = light;
+        float d = glm::distance(light->position, position);
+        size_t i = 0;
+        while (i < v.size() && i < maximumAmount) {
+            if (glm::distance(v[i]->position, position) > d) {
+                PointLight *tmp = v[i];
+                v[i] = l;
+                l = tmp;
+            }
+            i++;
+        }
+
+        if (v.size() < maximumAmount) v.push_back(l);
+    }
+
+    return v;
+}
