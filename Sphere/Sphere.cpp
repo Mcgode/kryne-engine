ee22f37ab816bdb71d49193e95e6e2c143f7b821
Max Godefroy (Bragg2X) <max@godefroy.net>
2019-06-09 10:26:19 +0200

    Sphere - Updated material shininess and specular value

--- Sphere/Sphere.cpp
+++ Sphere/Sphere.cpp
@@ -29,10 +29,10 @@ Sphere::Sphere() {
 
 void Sphere::draw(glm::mat4 projection, glm::mat4 view, glm::mat4 model, AdditionalParameters *params)
 {
-    shader->setFloat("material.shininess", 12.0f);
+    shader->setFloat("material.shininess", 64.0f);
     shader->setVec3("material.ambient", glm::vec3(0.07));
     shader->setVec3("material.diffuse", glm::vec3(1.0));
-    shader->setVec3("material.specular", glm::vec3(0.8));
+    shader->setVec3("material.specular", glm::vec3(1.6));
     diffuseTexture->setTexture(shader, 0, "material.diffuseMap");
     normalMapTexture->setTexture(shader, 1, "material.normalMap");
     BaseObject::draw(projection, view, model, params);
