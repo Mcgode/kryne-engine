c72a460ec538224d6e0e3480c8c9ac641de5deb4
Max Godefroy (Bragg2X) <max@godefroy.net>
2019-10-15 12:03:53 +0200

    SunLight - Fixed floor shadow bias

--- SunLight/Floor.cpp
+++ SunLight/Floor.cpp
@@ -40,7 +40,7 @@ void Floor::draw(glm::mat4 projection, glm::mat4 view, glm::mat4 model, Addition
     shader->setVec3("material.specular", glm::vec3(1.6));
     Texture2D::textureSet(this->handler->getDirectionalShadowMap(this->light), shader, "directionalShadowMap.shadowMap");
     shader->setMat4("directionalShadowMap.lightSpaceMatrix", handler->getLightSpaceMatrix(this->light));
-    shader->setFloat("directionalShadowMap.shadowBias", 0.0);
+    shader->setFloat("directionalShadowMap.shadowBias", 0.001);
     this->diffuseTexture->setTexture(shader, "material.diffuseMap");
     this->normalMapTexture->setTexture(shader, "material.normalMap");
     BaseObject::draw(projection, view, model, params);
