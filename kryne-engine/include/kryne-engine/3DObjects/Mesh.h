3216a813deab8852d7683b80dbed7fc3486a2623
Max Godefroy (Clyde) <max@godefroy.net>
2020-11-22 11:45:07 +0100

    Basic Mesh

---
+++ kryne-engine/include/kryne-engine/3DObjects/Mesh.h
@@ -0,0 +1,77 @@
+//
+// Created by Max Godefroy on 21/11/2020.
+//
+
+#ifndef INC_3D_DEMOS_MESH_H
+#define INC_3D_DEMOS_MESH_H
+
+#include <kryne-engine/Geometry/BufferGeometry.h>
+#include <kryne-engine/Material/Material.h>
+
+#include "Object3D.h"
+
+
+using namespace std;
+
+/**
+ * A 3D object that can be rendered. <br>
+ * Its geometry and material are saved as shared pointers because sharing the same geometry and/or material between
+ * multiple meshes is an expected behaviour and an engine feature.
+ */
+class Mesh: public Object3D {
+
+public:
+
+    /**
+     * Initializes a mesh.
+     * @param geometry  The geometry for this mesh.
+     * @param material  The material for this mesh.
+     */
+    Mesh(const shared_ptr<BufferGeometry> &geometry, const shared_ptr<Material> &material) :
+        Object3D(),
+        geometry(geometry),
+        material(material)
+        {};
+
+    /**
+     * Returns the current geometry in the form of a shared pointer.
+     */
+    [[nodiscard]] const shared_ptr<BufferGeometry> &getGeometry() const {
+        return geometry;
+    }
+
+    /**
+     * Overrides the current mesh geometry.
+     * @param newGeometry   The new geometry for this mesh.
+     */
+    void setGeometry(const shared_ptr<BufferGeometry> &newGeometry) {
+        Mesh::geometry = newGeometry;
+    }
+
+    /**
+     * Returns the current material in the form of a shared pointer.
+     */
+    [[nodiscard]] const shared_ptr<Material> &getMaterial() const {
+        return material;
+    }
+
+    /**
+     * Overrides the current mesh material.
+     * @param newMaterial   The new material for this mesh.
+     */
+    void setMaterial(const shared_ptr<Material> &newMaterial) {
+        Mesh::material = newMaterial;
+    }
+
+protected:
+
+    /// The mesh geometry
+    shared_ptr<BufferGeometry> geometry;
+
+    /// The mesh material
+    shared_ptr<Material> material;
+
+};
+
+
+#endif //INC_3D_DEMOS_MESH_H
---
+++ kryne-engine/src/3DObjects/Mesh.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Max Godefroy on 21/11/2020.
+//
+
+#include "kryne-engine/3DObjects/Mesh.h"
