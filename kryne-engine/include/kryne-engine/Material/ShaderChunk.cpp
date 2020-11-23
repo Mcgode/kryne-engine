77f7f1853a1a164f355011b0f805f138f21c1af2
Max Godefroy (Clyde) <max@godefroy.net>
2020-11-23 20:35:53 +0100

    Implemented basic ShaderChunk cache

---
+++ kryne-engine/include/kryne-engine/Material/ShaderChunk.cpp
@@ -0,0 +1,49 @@
+//
+// Created by Max Godefroy on 23/11/2020.
+//
+
+#include "ShaderChunk.h"
+
+#ifndef _WIN32
+#define DIR "Resources/Shaders/"
+#else
+#define DIR "Resources\\Shaders\\"
+#endif
+
+
+const string &ShaderChunk::getCodeChunk(const string &filename)
+{
+    auto location = this->shaderCodeCache.find(filename);
+    if (location != this->shaderCodeCache.end())
+        return location->second;
+    auto code = loadCodeFile(filename);
+    this->shaderCodeCache.emplace(filename, code);
+    return this->shaderCodeCache.at(filename);
+}
+
+
+string ShaderChunk::loadCodeFile(const string &filename) {
+
+    string shaderCode;
+
+    // Saving full filename
+    string fullFilename = DIR + filename + ".glsl";
+
+    // Opening read stream
+    ifstream file(fullFilename);
+
+    // Check if file was found
+    if (!file) {
+        std::cerr << "ERROR::SHADER::SHADER_PROGRAM_RECOVERY_FAILED\nCould not find file at " << fullFilename.c_str() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Parsing source code into a string
+    file.seekg(0, std::ios::end); // Setting cursor position to file end
+    shaderCode.resize(file.tellg()); // Getting value of last position from the current cursor position and setting the string size accordingly
+    file.seekg(0, std::ios::beg); // Resetting the cursor position to the beginning
+    file.read(&shaderCode[0], shaderCode.size()); // Copying whole file to string
+    file.close();
+
+    return shaderCode;
+}
---
+++ kryne-engine/include/kryne-engine/Material/ShaderChunk.h
@@ -0,0 +1,67 @@
+//
+// Created by Max Godefroy on 23/11/2020.
+//
+
+#ifndef INC_3D_DEMOS_SHADERCHUNK_H
+#define INC_3D_DEMOS_SHADERCHUNK_H
+
+#include <unordered_map>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+/**
+ * A singleton class for handling the retrieval and caching of shader code chunks
+ */
+class ShaderChunk {
+
+public:
+
+    /**
+     * Get the ShaderChunk instance.
+     *
+     * @note Thread safety of the initialization operation is assured by using a Meyers singleton pattern.
+     */
+    static ShaderChunk &getInstance() {
+        static ShaderChunk instance;
+        return instance;
+    }
+
+    /**
+     * Retrieves the shader code chunk corresponding to the provided filename.
+     *
+     * @param filename  The name of the shader file.
+     * @return A string containing the shader code.
+     *
+     * @note Uses a cache system to avoid repeated access cost due to IO operations.
+     *
+     * @warning Might not be a thread safe operation
+     */
+    const string &getCodeChunk(const string &filename);
+
+private:
+
+    /**
+     * Perform IO operation to retrieve the code chunk, when encountering a cache miss.
+     * @param filename  The name of the file to retrieve from.
+     * @return A string containing the shader code.
+     */
+    static string loadCodeFile(const string &filename);
+
+private:
+
+    /**
+     * The cache map for all the retrieved code chunks.
+     *
+     * @note Since the shader chunks are a static set of files, memory isn't much of a problem, and, as such, a cache
+     *       expiry system was deemed unnecessary.
+     */
+    unordered_map<string, string> shaderCodeCache {};
+
+};
+
+
+#endif //INC_3D_DEMOS_SHADERCHUNK_H
