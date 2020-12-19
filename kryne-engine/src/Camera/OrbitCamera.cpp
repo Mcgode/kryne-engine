44ddcaa78cc56851d983360fcf37f5909b461aba
Max Godefroy (Clyde) <max@godefroy.net>
2020-12-19 13:43:26 +0100

    OrbitCamera - Screen-space drag

--- kryne-engine/src/Camera/OrbitCamera.cpp
+++ kryne-engine/src/Camera/OrbitCamera.cpp
@@ -20,6 +20,9 @@ void OrbitCamera::update(bool force)
 {
     const auto input = this->playerInput.lock();
 
+    bool needAngleUpdate = false;
+    bool needCenterUpdate = false;
+
     if (input != nullptr)
     {
         const auto dir = this->position - this->centerPosition;
@@ -27,22 +30,54 @@ void OrbitCamera::update(bool force)
 
         const auto movement = input->getCursorMovement();
         auto newAngle = this->angle;
+        auto newCenter = this->centerPosition;
 
-        if (input->isKeyDown(GLFW_MOUSE_BUTTON_1)) {
-            newAngle.x += -movement.x * this->radiansPerMousePixel;
-            newAngle.y += movement.y * this->radiansPerMousePixel;
-            newAngle.y = glm::min(glm::max(newAngle.y, -this->maxPhi), this->maxPhi);
+        if (movement.x != 0 && movement.y != 0)
+        {
+            if (input->isKeyDown(GLFW_MOUSE_BUTTON_1))
+            {
+                needAngleUpdate = true;
+                newAngle.x += -movement.x * this->radiansPerMousePixel;
+                newAngle.y += movement.y * this->radiansPerMousePixel;
+                newAngle.y = glm::min(glm::max(newAngle.y, -this->maxPhi), this->maxPhi);
+            }
+            else if (input->isKeyDown(GLFW_MOUSE_BUTTON_2))
+            {
+                needCenterUpdate = true;
+
+                const vec3 up(0, 1, 0);
+                auto z = normalize(this->position - newCenter);
+                auto x = cross(up, z);
+
+                if (glm::length(x) == 0.f) {
+                    if (glm::abs(z.z) == 1.0f)
+                        z.x += 0.0001f;
+                    else
+                        z.z += 0.0001f;
+
+                    z = glm::normalize(z);
+                    x = glm::normalize(glm::cross(up, z));
+                } else
+                    x = glm::normalize(x);
+
+                auto y = glm::cross(z, x);
+
+                const float dx = -movement.x * this->radiansPerMousePixel;
+                const float dy = movement.y * this->radiansPerMousePixel;
+                newCenter += dx * x + dy * y;
+            }
         }
 
-        if (this->angle != newAngle)
+        if (needAngleUpdate || needCenterUpdate)
         {
             this->angle = newAngle;
+            this->centerPosition = newCenter;
             this->updatePosition();
         }
 
     }
 
-    Camera::update(force);
+    Camera::update(force || needAngleUpdate || needCenterUpdate);
 }
 
 void OrbitCamera::setPosition(const vec3 &pos)
