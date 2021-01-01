941f801f9b9710068d1c296e6db1e67461cc1ecd
Max Godefroy (Clyde) <max@godefroy.net>
2021-01-01 14:50:56 +0100

    Process - Attach/detach entity

--- kryne-engine/include/kryne-engine/Core/Process.h
+++ kryne-engine/include/kryne-engine/Core/Process.h
@@ -11,6 +11,7 @@ class Entity;
 
 
 #include <memory>
+#include <unordered_map>
 #include "Entity.h"
 
 
@@ -21,12 +22,37 @@ class Process {
 
 public:
 
+    /**
+     * Initializes an entity and attaches it to this process.
+     * @tparam T        The entity class
+     * @tparam Args     The class constructor argument types collection.
+     * @param args      The arguments for the class constructor
+     * @return A pointer to the newly created entity. The pointer is linked to a shared pointer owned only by the
+     *         process. Weak reference to the object can be retrieved using #getWeakReference
+     */
     template<typename T, typename... Args>
-    inline unique_ptr<T> makeEntity(Args&&... args)
-    {
-        static_assert(is_constructible<T, Entity>(), "Class must inherit from Entity");
-        return make_unique<T>(this, forward<Args>(args)...);
-    }
+    inline T *makeEntity(Args&&... args);
+
+    /**
+     * Retrieves a weak reference of an entity attached to the process.
+     * @param entity    The attached entity.
+     * @return  A weak reference to the provided entity. Will link to nullptr if the provided entity is not attached to
+     *          the process.
+     */
+    weak_ptr<Entity *> getWeakReference(Entity *entity);
+
+    /**
+     * Detaches an entity from the process, meaning it should be deleted (if there is no other shared reference active
+     * at the moment).
+     * @param entity    The entity to detach.
+     * @return `true` if the provided entity was attached. `false` otherwise.
+     */
+    bool detachEntity(Entity *entity);
+
+protected:
+
+    /// The set of entities attached to this process, mapping the pointer to its corresponding shared pointer.
+    unordered_map<Entity *, shared_ptr<Entity>> processEntities;
 
 };
 
--- kryne-engine/src/Core/Process.cpp
+++ kryne-engine/src/Core/Process.cpp
@@ -5,3 +5,39 @@
  */
 
 #include "kryne-engine/Core/Process.h"
+
+template<typename T, typename... Args>
+T *Process::makeEntity(Args &&... args)
+{
+    static_assert(is_convertible<T, Entity>::value, "Class must inherit from Entity");
+
+    const auto entity = make_shared<T>(this, forward<Args>(args)...);
+    this->processEntities.emplace(pair(entity->get(), entity));
+    return entity->get();
+}
+
+
+weak_ptr<Entity *> Process::getWeakReference(Entity *entity)
+{
+    const auto it = this->processEntities.find(entity);
+
+    weak_ptr<Entity *> ptr;
+    if (it != this->processEntities.end())
+        ptr = it->second;
+
+    return ptr;
+}
+
+
+bool Process::detachEntity(Entity *entity)
+{
+    const auto it = this->processEntities.find(entity);
+
+    if (it != this->processEntities.end())
+    {
+        this->processEntities.erase(it);
+        return true;
+    }
+
+    return false;
+}
