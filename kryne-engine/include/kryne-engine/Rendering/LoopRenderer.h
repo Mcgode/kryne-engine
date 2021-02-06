1091d5d877ba9047ab69f6515f5337775e939485
Max Godefroy (Clyde) <max@godefroy.net>
2021-02-06 17:07:08 +0100

    LoopRenderer - Implementation

---
+++ kryne-engine/include/kryne-engine/Rendering/LoopRenderer.h
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 06/02/2021.
+ */
+
+#pragma once
+
+
+class RenderMesh;
+class Camera;
+class Scene;
+
+
+/**
+ * A renderer that will be run in the game loop.
+ */
+class LoopRenderer {
+
+public:
+
+    virtual void handleMesh(RenderMesh *renderMesh) = 0;
+
+    virtual void parseScene(Scene *scene) = 0;
+
+    virtual void setCamera(weak_ptr<Camera> camera) = 0;
+
+};
+
+
+#include "RenderMesh.h"
