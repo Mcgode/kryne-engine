2559b16be8a179b47dd6fb0cbe91d2779c71e6b9
Max Godefroy (Clyde) <max@godefroy.net>
2021-02-06 17:04:44 +0100

    RenderMesh - Added functions

--- kryne-engine/include/kryne-engine/Rendering/RenderMesh.h
+++ kryne-engine/include/kryne-engine/Rendering/RenderMesh.h
@@ -9,4 +9,64 @@
 
 #include <kryne-engine/Core/Component.h>
 
-class RenderMesh: public Component {};
+class Entity;
+class Camera;
+
+
+#include <kryne-engine/Geometry/BufferGeometry.h>
+#include <kryne-engine/Material/Material.h>
+
+
+class RenderMesh: public Component {
+
+public:
+
+    RenderMesh(Entity *entity, shared_ptr<BufferGeometry> geometry, shared_ptr<Material> material);
+
+    /**
+     * Function called before the mesh is rendered
+     * @param camera    The camera used for the rendering
+     */
+    virtual void onBeforeRender(const Camera *camera) {};
+
+    /**
+     * Returns the current geometry in the form of a shared pointer.
+     */
+    [[nodiscard]] const shared_ptr<BufferGeometry> &getGeometry() const {
+        return geometry;
+    }
+
+    /**
+     * Overrides the current mesh geometry.
+     * @param newGeometry   The new geometry for this mesh.
+     */
+    void setGeometry(const shared_ptr<BufferGeometry> &newGeometry) {
+        this->geometry = newGeometry;
+    }
+
+    /**
+     * Returns the current material in the form of a shared pointer.
+     */
+    [[nodiscard]] const shared_ptr<Material> &getMaterial() const {
+        return material;
+    }
+
+    /**
+     * Overrides the current mesh material.
+     * @param newMaterial   The new material for this mesh.
+     */
+    void setMaterial(const shared_ptr<Material> &newMaterial) {
+        this->material = newMaterial;
+    }
+
+protected:
+
+    /// The mesh geometry
+    shared_ptr<BufferGeometry> geometry;
+
+    /// The mesh material
+    shared_ptr<Material> material;
+
+};
+
+#include <kryne-engine/Camera/Camera.h>
--- kryne-engine/src/Rendering/RenderMesh.cpp
+++ kryne-engine/src/Rendering/RenderMesh.cpp
@@ -5,3 +5,10 @@
  */
 
 #include "kryne-engine/Rendering/RenderMesh.h"
+
+
+RenderMesh::RenderMesh(Entity *entity, shared_ptr<BufferGeometry> geometry, shared_ptr<Material> material):
+    Component(entity),
+    geometry(move(geometry)),
+    material(move(material))
+{}
