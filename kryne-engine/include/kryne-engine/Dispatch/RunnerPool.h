046684752cffd5c01e57c53ad359fce3bebe89b0
Max Godefroy (Clyde) <max@godefroy.net>
2021-02-25 21:00:55 +0100

    RunnerPool - Implementation

---
+++ kryne-engine/include/kryne-engine/Dispatch/RunnerPool.h
@@ -0,0 +1,78 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 25/02/2021.
+ */
+
+#pragma once
+
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <future>
+#include <memory>
+
+
+using namespace std;
+
+
+class RunnerPool {
+
+
+public:
+
+    explicit RunnerPool(uint16_t threadCount);
+
+    virtual ~RunnerPool();
+
+    template<class F, class... Args>
+    future<result_of_t<F(Args...)>> enqueue(F&& function, Args&& ...args)
+    {
+        using returnType = result_of_t<F(Args...)>;
+
+        auto task = make_shared<packaged_task<returnType()>>(
+            bind(forward<F>(function), forward<Args>(args)...)
+        );
+
+        future<returnType> result = task->get_future();
+        {
+            unique_lock<mutex> lock(this->poolMutex);
+
+            if (this->stop)
+                throw runtime_error("Tried to enqueue a stopped thread");
+
+            tasks.emplace([task] { task.get()(); });
+        }
+
+        waitCondition.notify_one();
+
+        return result;
+    }
+
+
+protected:
+
+    enum internal {};
+
+    RunnerPool(uint16_t threadCount, enum internal);
+
+
+protected:
+
+    uint16_t threadCount;
+
+    thread *threads;
+
+    queue<function<void()>> tasks {};
+
+    mutex poolMutex {};
+
+    condition_variable waitCondition {};
+
+    bool stop = false;
+
+};
+
+
---
+++ kryne-engine/src/Dispatch/RunnerPool.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 25/02/2021.
+ */
+
+#include "kryne-engine/Dispatch/RunnerPool.h"
+
+
+RunnerPool::RunnerPool(uint16_t threadCount, enum internal): threadCount(threadCount)
+{
+    this->threads = new thread[this->threadCount];
+}
+
+
+RunnerPool::RunnerPool(uint16_t threadCount): RunnerPool(threadCount, RunnerPool::internal())
+{
+    for (uint16_t i = 0; i < this->threadCount; i++)
+    {
+        threads[i] = thread(
+            [this]
+            {
+                for (;;)
+                {
+                    function<void()> task;
+                    {
+                        unique_lock<mutex> lock(this->poolMutex);
+
+                        waitCondition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
+
+                        if (this->stop && this->tasks.empty())
+                            return;
+
+                        task = move(this->tasks.front());
+                        this->tasks.pop();
+                    }
+                    task();
+                }
+            }
+        );
+    }
+}
+
+
+RunnerPool::~RunnerPool()
+{
+    {
+        unique_lock<mutex> lock(this->poolMutex);
+        this->stop = true;
+    }
+
+    this->waitCondition.notify_all();
+
+    for (uint16_t i = 0; i < this->threadCount; i++)
+        this->threads[i].join();
+
+    delete[] this->threads;
+}
