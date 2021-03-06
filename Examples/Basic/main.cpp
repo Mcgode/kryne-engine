9811a11654765c5debbe9fca4b71cb4d2ce85edf
Max Godefroy (Clyde) <max@godefroy.net>
2021-03-06 12:46:51 +0100

    Basic example - Test initialization of mesh in parallel thread

--- Examples/Basic/main.cpp
+++ Examples/Basic/main.cpp
@@ -26,34 +26,42 @@ int main()
     const auto normalMap = Texture2D::loadFromFileSync("Resources/Textures/cobblestone/cobblestone_floor_norm.jpg");
     const auto roughnessMap = Texture2D::loadFromFileSync("Resources/Textures/cobblestone/cobblestone_floor_rough.jpg");
 
+    // Uncomment to test multi-threaded texture loading
+//    shared_ptr<Texture2D> maps[50];
+//    for (int i = 0; i < 50; i++)
+//    {
+//        maps[i] = Texture2D::loadFromFileSync("Resources/Textures/cobblestone/cobblestone_floor_rough.jpg");
+//    }
+//    Dispatcher::instance().io()->enqueue([]()
+//    {
+//        cout << "IO operations should have concluded" << endl;
+//    });
+
     MeshStandardMaterial::StandardInitParameters params;
-    params.map = map;
-    params.normalMap = normalMap;
-    params.roughnessMap = roughnessMap;
-    params.metalness = 0.0;
-    auto material = make_shared<MeshStandardMaterial>(params);
-    material->setSide(FrontSide);
 
-    material->setDefine("MAX_DIRECTIONAL_LIGHTS", "1");
-    material->setUniform("directionalLights[0].color", glm::vec3(1));
-    material->setUniform("directionalLights[0].direction", glm::vec3(-1, -0.8, -0.5));
-
-    const float intensity = 0.2;
-    material->setDefine("MAX_HEMISPHERE_LIGHTS", "1");
-    material->setUniform("hemisphereLights[0].skyColor", intensity * glm::vec3(1));
-    material->setUniform("hemisphereLights[0].groundColor", intensity * glm::vec3(0.5));
-    material->setUniform("hemisphereLights[0].direction", glm::vec3(0, 1, 0));
-
-    Dispatcher::instance().enqueueParallelDelayed([&process, &material, &scene]()
+    Dispatcher::instance().enqueueParallelDelayed([&process, &params, &scene, map, normalMap, roughnessMap]()
     {
-        auto geometry = dynamic_pointer_cast<BufferGeometry>(make_shared<BoxBufferGeometry>());
+        params.map = map;
+        params.normalMap = normalMap;
+        params.roughnessMap = roughnessMap;
+        params.metalness = 0.0;
+        auto material = make_shared<MeshStandardMaterial>(params);
+        material->setSide(FrontSide);
 
-        Dispatcher::instance().enqueueMainDelayed([&process, &material, &scene, geometry]()
-        {
-            const auto entity = process->makeEntity<Entity>();
-            const auto mesh = entity->addComponent<RenderMesh>(geometry, material);
-            entity->getTransform()->setScene(scene);
-        });
+        material->setDefine("MAX_DIRECTIONAL_LIGHTS", "1");
+        material->setUniform("directionalLights[0].color", glm::vec3(1));
+        material->setUniform("directionalLights[0].direction", glm::vec3(-1, -0.8, -0.5));
+
+        const float intensity = 0.2;
+        material->setDefine("MAX_HEMISPHERE_LIGHTS", "1");
+        material->setUniform("hemisphereLights[0].skyColor", intensity * glm::vec3(1));
+        material->setUniform("hemisphereLights[0].groundColor", intensity * glm::vec3(0.5));
+        material->setUniform("hemisphereLights[0].direction", glm::vec3(0, 1, 0));
+
+        auto geometry = dynamic_pointer_cast<BufferGeometry>(make_shared<BoxBufferGeometry>());
+        const auto entity = process->makeEntity<Entity>();
+        const auto mesh = entity->addComponent<RenderMesh>(geometry, material);
+        entity->getTransform()->setScene(scene);
     });
 
     const auto camera = process->makeEntity<Camera>(make_unique<PerspectiveProjectionData>(16.f / 9.f));
