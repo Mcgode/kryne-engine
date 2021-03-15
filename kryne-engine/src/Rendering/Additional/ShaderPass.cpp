a18a72c0704e2f084142b475f31375823f39634d
Max Godefroy (Clyde) <max@godefroy.net>
2021-03-15 17:05:52 +0100

    Added basic ShaderPass

---
+++ kryne-engine/src/Rendering/Additional/ShaderPass.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 15/03/2021.
+ */
+
+#include "kryne-engine/Rendering/Additional/ShaderPass.h"
+
+ShaderPass::ShaderPass(const string &name, const shared_ptr<Material> &material,
+                       PostProcessPass::Priority priority)
+        : PostProcessPass(priority, name), material(material)
+{}
+
+
+void ShaderPass::processPass(LoopRenderer *renderer, Framebuffer *readBuffer, Framebuffer *writeBuffer)
+{
+    this->material->setUniform("tDiffuse", readBuffer->retrieveColor(0));
+
+    writeBuffer->setAsRenderTarget();
+    renderer->textureRender(this->material.get());
+}
