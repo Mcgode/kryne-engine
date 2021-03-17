6cf2c14cda199056b4c630f2c394c376fd46cc53
Max Godefroy (Clyde) <max@godefroy.net>
2021-03-17 12:25:25 +0100

    RenderMesh - Basic UI

--- kryne-engine/src/Rendering/RenderMesh.cpp
+++ kryne-engine/src/Rendering/RenderMesh.cpp
@@ -4,6 +4,8 @@
  * @date 31/01/2021.
  */
 
+#include <kryne-engine/Utils/DreamImGui.hpp>
+
 #include "kryne-engine/Rendering/RenderMesh.h"
 
 
@@ -23,4 +25,32 @@ void RenderMesh::transformDidUpdate()
         this->boundingSphere = this->geometry->getBoundingSphere().fromMatrix(transform->getWorldMatrix());
     else
         this->boundingSphere = Math::Sphere();
+}
+
+
+void RenderMesh::renderComponentDetails()
+{
+    using Utils::ImGuiLabel;
+
+    ImGui::Separator();
+
+    ImGui::Checkbox(ImGuiLabel("Frustum culled", this), &this->frustumCull);
+
+    if (this->geometry)
+    {
+        if (ImGui::TreeNode(this, "Geometry info"))
+        {
+            for (auto &line : this->geometry->getInfo())
+                ImGui::Text("%s", line.c_str());
+
+            ImGui::TreePop();
+        }
+    }
+    else
+    {
+        ImGui::TextDisabled("No geometry attached");
+    }
+
+    // TODO: Material UI
+
 }
