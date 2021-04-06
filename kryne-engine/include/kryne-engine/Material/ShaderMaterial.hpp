eac9e27be51c7c792243c82d141d57244fbf6e65
Max Godefroy (Clyde) <max@godefroy.net>
2021-04-06 22:19:04 +0200

    ShaderMaterial - Implementation

---
+++ kryne-engine/include/kryne-engine/Material/ShaderMaterial.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 06/04/2021.
+ */
+
+#pragma once
+
+
+#include "Material.h"
+
+
+/**
+ * @brief A wrapper material for directly using shaders
+ */
+class ShaderMaterial : public Material {
+
+public:
+
+    /**
+     * @brief Initializes the shader material.
+     *
+     * @param shader The shader for this material.
+     */
+    ShaderMaterial(unique_ptr<Shader> shader) : Material(move(shader)) {}
+
+    /// @brief Retrieves this material's name
+    const string &getName() const { return name; }
+
+    /// @brief Changes this material's name
+    void setName(const string &value) { ShaderMaterial::name = value; }
+
+protected:
+
+    // Override
+    string materialName() override { return this->name; }
+
+protected:
+
+    /// The name of this material
+    string name = "ShaderMaterial";
+
+};
+
+
