70cfd948a9318f3a83a5a6acf5371be3324a466d
Max Godefroy (Clyde) <max@godefroy.net>
2021-04-08 19:12:34 +0200

    EnvironmentMap - Code cleanup

--- kryne-engine/src/Textures/EnvironmentMap.cpp
+++ kryne-engine/src/Textures/EnvironmentMap.cpp
@@ -5,9 +5,3 @@
  */
 
 #include "kryne-engine/Textures/EnvironmentMap.hpp"
-
-void EnvironmentMap::setIBL(const shared_ptr<CubeTexture> &newEnvMap)
-{
-    this->iblReady = true;
-    this->environmentMap = newEnvMap;
-}
