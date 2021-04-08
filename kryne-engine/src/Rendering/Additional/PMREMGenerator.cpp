a420dbfadcdf2ffdf4e77fc6764cf090b6cf9d98
Max Godefroy (Clyde) <max@godefroy.net>
2021-04-08 12:28:35 +0200

    PMREMGenerator - Delegate CubeTexture initialization

--- kryne-engine/src/Rendering/Additional/PMREMGenerator.cpp
+++ kryne-engine/src/Rendering/Additional/PMREMGenerator.cpp
@@ -45,20 +45,15 @@ void PMREMGenerator::runProcessing(LoopRenderer *renderer)
         this->mapsToProcess.pop_front();
     }
 
-    GLuint irradianceMap;
-    glGenTextures(1, &irradianceMap);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, irradianceMap);
-    for (unsigned int i = 0; i < 6; ++i)
-    {
-        glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, 32, 32, 0, GL_RGB, GL_FLOAT, nullptr);
-    }
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    auto irradianceCubeTexture = make_shared<CubeTexture>(
+            ivec2(32),
+            Texture::Formats::RGB,
+            Texture::InternalSizes::HalfFloat,
+            Texture::DataTypes::Float
+    );
 
-    auto irradianceCubeTexture = make_shared<CubeTexture>(irradianceMap);
+    irradianceCubeTexture->setWrap(GL_CLAMP_TO_EDGE);
+    irradianceCubeTexture->setFiltering(GL_LINEAR, GL_LINEAR);
 
     auto irradianceShader = make_unique<Shader>("Engine/IrradianceConvolution");
     auto irradianceMaterial = make_unique<ShaderMaterial>(move(irradianceShader));
