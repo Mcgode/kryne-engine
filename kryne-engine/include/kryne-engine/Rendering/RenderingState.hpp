b5faa837a98e92c4ee39f30ed18e1468b283254f
Max Godefroy (Clyde) <max@godefroy.net>
2021-04-16 07:51:54 +0200

    RenderingState - glScissor

--- kryne-engine/include/kryne-engine/Rendering/RenderingState.hpp
+++ kryne-engine/include/kryne-engine/Rendering/RenderingState.hpp
@@ -132,6 +132,67 @@ private:
 
     glm::ivec2 viewportSize {};
 
+
+// --- Scissoring ---
+
+public:
+
+    /// @brief Retrieves scissor rectangle start
+    [[nodiscard]] const glm::ivec2 &getScissorStart() const { return scissorStart; }
+
+    /// @brief Updates scissor rectangle start
+    void setScissorStart(const glm::ivec2 &newStart)
+    {
+        if (!glm::all(glm::equal(newStart, this->scissorStart)))
+            glScissor(newStart.x, newStart.y, this->scissorSize.x, this->scissorSize.y);
+        RenderingState::scissorStart = newStart;
+    }
+
+    /// @brief Retrieves scissor rectangle size
+    [[nodiscard]] const glm::ivec2 &getScissorSize() const { return scissorSize; }
+
+    /// @brief Updates scissor rectangle size
+    void setScissorSize(const glm::ivec2 &newSize)
+    {
+        if (!glm::all(glm::equal(newSize, this->scissorSize)))
+            glScissor(this->scissorStart.x, this->scissorStart.y, newSize.x, newSize.y);
+        RenderingState::scissorSize = newSize;
+    }
+
+    /// @brief Updates scissor rectangle
+    void setScissor(const glm::ivec2 &start, const glm::ivec2 &size)
+    {
+        if (!glm::all(glm::equal(size, this->scissorSize)) || !glm::all(glm::equal(start, this->scissorStart)))
+            glScissor(start.x, start.y, size.x, size.y);
+
+        this->scissorStart = start;
+        this->scissorSize = size;
+    }
+
+    /// @brief Toggles the scissoring test, and will update GL accordingly.
+    void setScissor(bool value)
+    {
+        if (value != scissorEnable)
+        {
+            if (value)
+                glEnable(GL_SCISSOR_TEST);
+            else
+                glDisable(GL_SCISSOR_TEST);
+        }
+        this->scissorEnable = value;
+    }
+
+private:
+
+    /// Whether scissor test is enabled or not
+    bool scissorEnable = false;
+
+    /// The scissoring rectangle start
+    glm::ivec2 scissorStart {};
+
+    /// The scissoring rectangle size
+    glm::ivec2 scissorSize {};
+
 };
 
 
