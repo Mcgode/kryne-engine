44ce8c4fa5a11abf3ae59e9c82aeb14bff403d5a
Max Godefroy (Clyde) <max@godefroy.net>
2021-04-17 22:38:46 +0200

    ShadowMappingProcess - Update directional light camera pointers

--- kryne-engine/src/Rendering/Processes/ShadowMappingProcess.cpp
+++ kryne-engine/src/Rendering/Processes/ShadowMappingProcess.cpp
@@ -11,6 +11,22 @@ vector<Camera *> ShadowMappingProcess::prepareFrame(const LoopRenderer *renderer
     swap(this->currentCDL, this->castingDirLights);
     this->castingDirLights.clear();
 
+    // Main camera call should be safe.
+    const auto mainCamera = renderer->getMainCamera();
+
+    for (const auto &light : this->currentCDL)
+    {
+        if (light->shadowMapData == nullptr)
+        {
+            // TODO : Instantiate ShadowMapData
+            continue;
+        }
+
+        auto &camera = light->shadowMapData->shadowCamera;
+        camera->getTransform()->unsafeSetParent(light->getTransform());
+        camera->unsafeSetPreprocessingRequirement(mainCamera);
+    }
+
     return vector<Camera *>();
 }
 
@@ -18,7 +34,16 @@ void ShadowMappingProcess::render(LoopRenderer *renderer,
                                   const std::vector<RenderMesh *> &meshes,
                                   const std::unordered_map<Camera *, LoopRenderer::FrustumCullingData> &frustumCullingMap)
 {
-
+    // Reset light cameras unsafe pointers.
+    // We do it at this point, because we're still sure that all the lights still exist.
+    for (const auto &light : this->currentCDL)
+    {
+        if (light->shadowMapData)
+        {
+            light->shadowMapData->shadowCamera->getTransform()->unsafeSetParent(nullptr);
+            light->shadowMapData->shadowCamera->unsafeSetPreprocessingRequirement(nullptr);
+        }
+    }
 }
 
 
