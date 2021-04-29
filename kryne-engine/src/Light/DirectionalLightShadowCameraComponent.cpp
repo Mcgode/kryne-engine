3b278394989670036258820ed5faddd87c850e7f
Max Godefroy (Clyde) <max@godefroy.net>
2021-04-29 09:47:14 +0200

    DirectionalLightShadowCameraComponent - Improved camera positioning algorithm

--- kryne-engine/src/Light/DirectionalLightShadowCameraComponent.cpp
+++ kryne-engine/src/Light/DirectionalLightShadowCameraComponent.cpp
@@ -4,13 +4,17 @@
  * @date 18/04/2021.
  */
 
+#include <kryne-engine/Math/Sphere.hpp>
+
 #include "kryne-engine/Light/DirectionalLightShadowCameraComponent.hpp"
+
 
 DirectionalLightShadowCameraComponent::DirectionalLightShadowCameraComponent(Entity *entity) : LogicComponent(entity)
 {
     if (dynamic_cast<Camera *>(entity) == nullptr)
         throw runtime_error("The attached entity should be a Camera");
 }
+
 
 void DirectionalLightShadowCameraComponent::onUpdate()
 {
@@ -31,32 +35,53 @@ void DirectionalLightShadowCameraComponent::onUpdate()
     auto rotationMatrix = glm::toMat4(this->getTransform()->getQuaternion());
 
     auto localMatrix = inverse(parent->getTransform()->getWorldMatrix() * rotationMatrix);
-    auto transformMatrix = localMatrix *
-                           this->mainCamera->getTransform()->getWorldMatrix() *
-                           this->mainCamera->getInverseProjectionMatrix();
+    auto worldMatrix = inverse(this->mainCamera->getProjectionMatrix() * this->mainCamera->getViewMatrix());
+
+    float z = 1;
+    parent->maxShadowDistance = 15.f;
+    if (parent->maxShadowDistance != 0.f)
+    {
+        auto m = mainCamera->getInverseProjectionMatrix();
+        auto v1 = m * vec4(0, 0, -1, 1),
+             v2 = m * vec4(0, 0,  1, 1);
+        auto p1 = vec3(v1) / v1.w,
+             p2 = vec3(v2) / v2.w;
+        float depth = glm::distance(p1, p2);
+        float a = glm::min(parent->maxShadowDistance, depth) / depth;
+        z = glm::mix(-1.f, 1.f, a);
+    }
 
     vec3 frustumLocalPoints[] = {
         vec3(-1, -1, -1),
         vec3( 1, -1, -1),
         vec3(-1,  1, -1),
         vec3( 1,  1, -1),
-        vec3(-1, -1,  1),
-        vec3( 1, -1,  1),
-        vec3(-1,  1,  1),
-        vec3( 1,  1,  1),
+        vec3(-1, -1,  z),
+        vec3( 1, -1,  z),
+        vec3(-1,  1,  z),
+        vec3( 1,  1,  z),
     };
 
-    vec4 p0_4 = transformMatrix * vec4(frustumLocalPoints[0], 1);
-    vec3 p0 = vec3(p0_4) / p0_4.w;
-    vec3 minV(p0), maxV(p0);
-
-    for (const auto &point : frustumLocalPoints)
+    vec3 frustumCenter(0);
+    for (auto &point : frustumLocalPoints)
     {
-        vec4 v = transformMatrix * vec4(point, 1);
-        vec3 p = vec3(v) / v.w;
-        maxV = glm::max(maxV, p);
-        minV = glm::min(minV, p);
+        vec4 v = worldMatrix * vec4( point, 1.f );
+        point = vec3(v) / v.w;
+        frustumCenter += point * 0.125f;
     }
 
-    projectionData->setFrustum(minV, maxV);
+    float radius = std::numeric_limits<float>::min();
+    for (const auto &point : frustumLocalPoints)
+    {
+        float distance = glm::length(point - frustumCenter);
+        radius = glm::max(radius, distance);
+    }
+
+    Math::Sphere sphere(radius, frustumCenter);
+    const auto orthoSphere = sphere.fromMatrix(localMatrix);
+
+    vec3 minOrtho = orthoSphere.center - orthoSphere.radius;
+    vec3 maxOrtho = orthoSphere.center + orthoSphere.radius;
+
+    projectionData->setFrustum(minOrtho, maxOrtho);
 }
