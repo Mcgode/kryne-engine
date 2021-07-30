6250eaff5fe86db8b1ef1c5e2f35b514e8990937
Max Godefroy <max@godefroy.net>
2021-07-30 19:21:05 +0200

    Started setting up OpenGL context

---
+++ kryne-engine/graphics/opengl/GlGraphicsApi.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file
+ * @author max
+ * @date 7/30/21.
+ */
+
+#include "GlGraphicsApi.hpp"
---
+++ kryne-engine/graphics/opengl/GlGraphicsApi.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @author max
+ * @date 7/30/21.
+ */
+
+#pragma once
+#include <core/GraphicsApiContext.hpp>
+
+namespace KryneEngine
+{
+    class GlApiContext: public GraphicsApiContext
+    {
+
+    };
+}
+
+
