ab726ee6def1368230fc2e115c04ab08f6538b3a
Max Godefroy <max@godefroy.net>
2021-07-31 13:11:24 +0200

    CommonTypes

---
+++ kryne-engine/common/CommonTypes.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * @author max
+ * @date 7/31/21.
+ */
+
+#pragma once
+
+
+namespace KryneEngine
+{
+    using i8 = char;
+    using u8 = unsigned char;
+
+    using i16 = short;
+    using u16 = unsigned short;
+
+    using i32 = int;
+    using u32 = unsigned int;
+
+    using i64 = long long;
+    using u64 = unsigned long long;
+}
