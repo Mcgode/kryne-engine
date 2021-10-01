631fdedcc3f73824723b7ba76017856d2e1d627a
Max Godefroy <max@godefroy.net>
2021-10-01 18:10:26 +0200

    Changed type

--- kryne-engine/common/CommonTypes.hpp
+++ kryne-engine/common/CommonTypes.hpp
@@ -9,15 +9,15 @@
 
 namespace KryneEngine
 {
-    using i8 = char;
+    using s8 = char;
     using u8 = unsigned char;
 
-    using i16 = short;
+    using s16 = short;
     using u16 = unsigned short;
 
-    using i32 = int;
+    using s32 = int;
     using u32 = unsigned int;
 
-    using i64 = long long;
+    using s64 = long long;
     using u64 = unsigned long long;
 }
