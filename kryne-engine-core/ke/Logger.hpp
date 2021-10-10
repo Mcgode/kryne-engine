f4fc4b887ac391409a361b54420efa4192d646a7
Max Godefroy (Clyde) <max@godefroy.net>
2021-10-10 22:21:30 +0200

    Logger - LOG_IF

--- kryne-engine-core/ke/Logger.hpp
+++ kryne-engine-core/ke/Logger.hpp
@@ -15,11 +15,9 @@
 
 namespace KryneEngine
 {
-    class Logger
-    {
+    class Logger {
     public:
-        enum class Level
-        {
+        enum class Level {
             Verbose,
             Info,
             Warning,
@@ -27,10 +25,11 @@ namespace KryneEngine
             Fatal
         };
 
-        static void Log(Level _level, const eastl::string_view& _cat, const eastl::string_view& _message);
+        static void Log(Level _level, const eastl::string_view &_cat, const eastl::string_view &_message);
 
         template<class... Args>
-        static inline void Log(Level _level, const eastl::string_view& _cat, const eastl::string_view& _message, Args&&... _args)
+        static inline void
+        Log(Level _level, const eastl::string_view &_cat, const eastl::string_view &_message, Args &&... _args)
         {
 #if defined(ENABLE_LOGGING)
             auto string = eastl::string().sprintf(_message.begin(), eastl::forward<Args>(_args)...);
@@ -38,8 +37,7 @@ namespace KryneEngine
 #endif
         }
 
-        enum class CommonCategories
-        {
+        enum class CommonCategories {
             Core,
             Rendering,
         };
@@ -47,7 +45,8 @@ namespace KryneEngine
         static void Log(Level _level, CommonCategories _cat, const eastl::string_view &_message);
 
         template<class... Args>
-        static inline void Log(Level _level, CommonCategories _cat, const eastl::string_view& _message, Args&&... _args)
+        static inline void
+        Log(Level _level, CommonCategories _cat, const eastl::string_view &_message, Args &&... _args)
         {
 #if defined(ENABLE_LOGGING)
             auto string = eastl::string().sprintf(_message.begin(), eastl::forward<Args>(_args)...);
@@ -55,12 +54,19 @@ namespace KryneEngine
 #endif
         }
 
-#define LOG(level, cat, message,...) Logger::Log(Logger::Level::level, cat, message, ##__VA_ARGS__)
-#define LOG_VERBOSE(cat, message,...) LOG(Verbose, cat, message, ##__VA_ARGS__)
-#define LOG_INFO(cat, message,...) LOG(Info, cat, message, ##__VA_ARGS__)
-#define LOG_WARNING(cat, message,...) LOG(Warning, cat, message, ##__VA_ARGS__)
-#define LOG_ERROR(cat, message,...) LOG(Error, cat, message, ##__VA_ARGS__)
-#define LOG_FATAL(cat, ...) LOG(Fatal, cat, message, ##__VA_ARGS__)
+        #define LOG(level, cat, message, ...) Logger::Log(Logger::Level::level, cat, message, ##__VA_ARGS__)
+        #define LOG_VERBOSE(cat, message, ...) LOG(Verbose, cat, message, ##__VA_ARGS__)
+        #define LOG_INFO(cat, message, ...) LOG(Info, cat, message, ##__VA_ARGS__)
+        #define LOG_WARNING(cat, message, ...) LOG(Warning, cat, message, ##__VA_ARGS__)
+        #define LOG_ERROR(cat, message, ...) LOG(Error, cat, message, ##__VA_ARGS__)
+        #define LOG_FATAL(cat, ...) LOG(Fatal, cat, message, ##__VA_ARGS__)
+
+        #define LOG_IF(condition, level, cat, message, ...) if (condition) LOG(level, cat, message, ##__VA_ARGS__)
+        #define LOG_IF_VERBOSE(condition, cat, message, ...) LOG_IF(condition, Verbose, cat, message, ##__VA_ARGS__)
+        #define LOG_IF_INFO(condition, cat, message, ...) LOG_IF(condition, Info, cat, message, ##__VA_ARGS__)
+        #define LOG_IF_WARNING(condition, cat, message, ...) LOG_IF(condition, Warning, cat, message, ##__VA_ARGS__)
+        #define LOG_IF_ERROR(condition, cat, message, ...) LOG_IF(condition, Error, cat, message, ##__VA_ARGS__)
+        #define LOG_IF_FATAL(condition, cat, message, ...) LOG_IF(condition, Fatal, cat, message, ##__VA_ARGS__)
     };
 }
 
